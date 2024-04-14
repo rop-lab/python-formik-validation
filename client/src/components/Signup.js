5c2ee8a595ef584f41f351e89c95333e9d5f6493
rop-lab <jhnycheru11@gmail.com>
2024-04-14 11:45:38 +0300

    done

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -53,7 +53,7 @@ export const SignupForm = () => {
 
   return (
     <div>
-      <h1>Customer sign up form</h1>
+      <h1>Sign up form</h1>
       <form onSubmit={formik.handleSubmit} style={{ margin: "30px" }}>
         <label htmlFor="email">Email Address</label>
         <br />
